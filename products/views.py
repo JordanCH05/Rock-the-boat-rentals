6f7d030c74d3fe500186f183a493ae0dad66199a
JordanCH05 <jordi.caristiahill@gmail.com>
2022-04-08 19:29:41 +0000

    Add pagination and rearrange template

--- products/views.py
+++ products/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Boat
 
 
 def all_products(request):
     """ A view to show all boats """
 
-    boats = Boat.objects.all()
+    boat_list = Boat.objects.all()
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(boat_list, 40)
+    try:
+        boats = paginator.page(page)
+    except PageNotAnInteger:
+        boats = paginator.page(1)
+    except EmptyPage:
+        boats = paginator.page(paginator.num_pages)
 
     context = {
         'boats': boats,
