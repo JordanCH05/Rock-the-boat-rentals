31e849d5e71088030f5b74afbe069d2739dbd364
JordanCH05 <jordi.caristiahill@gmail.com>
2022-04-09 14:10:54 +0000

    Add pagination

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Boat
 
 
@@ -7,18 +7,25 @@ def all_products(request):
     """ A view to show all boats """
 
     boat_list = Boat.objects.all()
-    page = request.GET.get('page', 1)
-
     paginator = Paginator(boat_list, 40)
-    try:
-        boats = paginator.page(page)
-    except PageNotAnInteger:
-        boats = paginator.page(1)
-    except EmptyPage:
-        boats = paginator.page(paginator.num_pages)
 
+    page = request.GET.get('page')
+
+    try:
+        boats = paginator.get_page(page)
+    except PageNotAnInteger:
+        boats = paginator.get_page(1)
+    except EmptyPage:
+        boats = paginator.get_page(paginator.num_pages)
+
+    nums = "a" * boats.paginator.num_pages
+    page_min = boats.number - 5
+    page_max = boats.number + 5
+    page_range = range(page_min, page_max)
     context = {
         'boats': boats,
+        'nums': nums,
+        'page_range': page_range,
     }
 
     return render(request, 'products/products.html', context)
