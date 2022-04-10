7b140fa839034e354d7be7158367041de4fcb00e
JordanCH05 <jordi.caristiahill@gmail.com>
2022-04-10 14:55:17 +0000

    Add product detail page

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('<int:boat_id>/', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Boat
 
@@ -29,3 +29,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, boat_id):
+    """ A view to show individual boat details """
+
+    boat = get_object_or_404(Boat, pk=boat_id)
+
+    context = {
+        'boat': boat
+    }
+
+    return render(request, 'products/product_detail.html', context)
