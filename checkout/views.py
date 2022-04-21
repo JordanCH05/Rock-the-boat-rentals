866442a47836d3dedfac944b9b9ba5459f79f440
JordanCH05 <jordi.caristiahill@gmail.com>
2022-04-21 11:58:16 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KRDiPEs7dUZiWua0gDCmiq735AHBG23Cm9XxGPa1VU4yM5czJc1DdRLx1HJ2qoQusJRO5oGj41ULezFqxwuzRpT00OdiKN3Co',
+        'client_secret': 'test',
     }
 
     return render(request, template, context)
