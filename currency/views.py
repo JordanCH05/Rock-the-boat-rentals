1e1ef409734084672ac9b1304ed5302a97807d05
JordanCH05 <jordi.caristiahill@gmail.com>
2022-04-23 17:25:40 +0000

    Prevent currency change when boat in fleet

--- currency/views.py
+++ currency/views.py
@@ -1,21 +1,29 @@
 from django.shortcuts import redirect
 from django.contrib import messages
 from currencies.models import Currency
+from bag.contexts import fleet_contents
 
 
 def change_currency(request, currency, redirect_url=''):
 
     # If boat in fleet then prevent currency change
-
+    fleet_items = fleet_contents(request)['fleet_items']
     redirect_url = '/' + redirect_url
-    currencies = []
-    filt_cur = Currency.objects.values()
-    for cur in filt_cur:
-        currencies.append(cur['code'])
-    if currency in currencies:
-        request.session['currency'] = currency
-        messages.success(request, f"Currency changed to {currency}")
+    if fleet_items:
+        messages.warning(
+            request,
+            "Please empty your empty your fleet before changing currencies")
         return redirect(redirect_url)
     else:
-        messages.error(request, f"Sorry, we don't use this currency: {currency}")
-        return redirect(redirect_url)
+        currencies = []
+        filt_cur = Currency.objects.values()
+        for cur in filt_cur:
+            currencies.append(cur['code'])
+        if currency in currencies:
+            request.session['currency'] = currency
+            messages.success(request, f"Currency changed to {currency}")
+            return redirect(redirect_url)
+        else:
+            messages.error(request,
+                           f"Sorry, we don't use this currency: {currency}")
+            return redirect(redirect_url)
