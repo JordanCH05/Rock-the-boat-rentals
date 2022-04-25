9242debcc0babd53e31030d99d9df7cf52af9e22
JordanCH05 <jordi.caristiahill@gmail.com>
2022-04-25 11:32:28 +0000

    Add profiles admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    """ Profile Admin """
+
+    readonly_fields = (
+        'user',
+        )
+
+    fields = (
+        'user',
+        'default_phone_number',
+        'default_street_address1',
+        'default_street_address2',
+        'default_town_or_city',
+        'default_county',
+        'default_postcode',
+        'default_country',
+    )
+
+    list_display = (
+        'user',
+        'default_phone_number',
+        'default_street_address1',
+        'default_street_address2',
+        'default_town_or_city',
+        'default_county',
+        'default_postcode',
+        'default_country',
+    )
+
+
+admin.site.register(UserProfile)
