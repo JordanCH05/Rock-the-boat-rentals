4768381005f29aa3fd2666a28ee30d7349b8201a
JordanCH05 <jordi.caristiahill@gmail.com>
2022-04-27 21:17:21 +0000

    Create js file for add and edit reviews

---
+++ static/js/add_edit_review_stars.js
@@ -0,0 +1,8 @@
+$("#id_score")[0].addEventListener('input', function() {
+    var score_input = $("#id_score")[0].value;
+    var stars = $('.stars-inner');
+    var rating = (score_input / 5) * 100;
+    var ratingRounded = `${Math.round
+    ((rating) / 10) * 10}%`;
+    stars[0].style.width = ratingRounded;
+})
