3891a1f1338ede48ca5da199585b04d832d94b1b
JordanCH05 <jordi.caristiahill@gmail.com>
2022-04-27 21:12:38 +0000

    Adjust score form to choicefield and add stars javascript

--- reviews/forms.py
+++ reviews/forms.py
@@ -3,11 +3,18 @@ from crispy_forms.helper import FormHelper
 
 from .models import Review
 
+SCORE_CHOICES = [
+    (5, '5 / 5 - Amazing'),
+    (4, '4 / 5 - Good'),
+    (3, '3 / 5 - OK'),
+    (2, '2 / 5 - Bad'),
+    (1, '1 / 5 - Terrible'),
+    (0, '0 / 5 - Worst Boat Ever'),
+]
+
 
 class ReviewForm(forms.ModelForm):
     """ Review Form """
-
-    score = forms.IntegerField(min_value=1, max_value=5)
 
     class Meta:
         model = Review
@@ -18,3 +25,5 @@ class ReviewForm(forms.ModelForm):
         super(ReviewForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_show_labels = False
+
+        self.fields['score'] = forms.ChoiceField(choices=SCORE_CHOICES)
