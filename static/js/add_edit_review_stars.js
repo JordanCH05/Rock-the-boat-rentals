37ed0b1e11a51e6e22ce65748bf86e4c51264f6d
JordanCH05 <jordi.caristiahill@gmail.com>
2022-04-28 16:50:38 +0000

    Fix stars js script

--- static/js/add_edit_review_stars.js
+++ static/js/add_edit_review_stars.js
@@ -1,8 +1,12 @@
-$("#id_score")[0].addEventListener('input', function() {
+document.addEventListener('DOMContentLoaded', starRating);
+
+$("#id_score")[0].addEventListener('input', starRating);
+
+function starRating() {
     var score_input = $("#id_score")[0].value;
     var stars = $('.stars-inner');
     var rating = (score_input / 5) * 100;
     var ratingRounded = `${Math.round
     ((rating) / 10) * 10}%`;
     stars[0].style.width = ratingRounded;
-})
+}
