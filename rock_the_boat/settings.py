15ed20e44627c32366101063ca30a12ce81eaff1
JordanCH05 <jordi.caristiahill@gmail.com>
2022-04-28 15:00:33 +0000

    Remove cloudinary from installed apps

--- rock_the_boat/settings.py
+++ rock_the_boat/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.humanize',
-    'cloudinary',
+    # 'cloudinary',
     'currencies',
     'allauth',
     'allauth.account',
@@ -189,6 +189,12 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'rock-the-boat'
     AWS_S3_REGION_NAME = 'eu-west-2'
