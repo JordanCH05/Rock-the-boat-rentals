b769b8a3f6c1c871a3f0ef4060ed7b18fd41685f
JordanCH05 <jordi.caristiahill@gmail.com>
2022-04-28 15:18:58 +0000

    Remove Cloudinary

--- products/models.py
+++ products/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from currencies.models import Currency
-from cloudinary.models import CloudinaryField
 
 
 class Category(models.Model):
@@ -42,7 +41,8 @@ class Boat(models.Model):
         max_digits=5, decimal_places=2, null=True, blank=True)
     material = models.CharField(max_length=254, null=True, blank=True)
     views = models.IntegerField(default=0, null=True, blank=True)
-    image = CloudinaryField('image', null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     dimensions = models.CharField(max_length=254, null=True, blank=True)
 
     class Meta:
